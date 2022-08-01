72dd8ce70e0663cd5573cbd1cdce19b1a0195ce8
ramirobarrasoto <rbarrasoto@gmail.com>
2022-08-01 10:47:31 -0300

    🐛 fix bugs lower case and more alternatives map

--- app.js
+++ app.js
@@ -1,53 +1,83 @@
-document.getElementById("formWords").addEventListener("submit", saveTask)
+document.getElementById('formWords').addEventListener('submit', saveTask);
 
 function saveTask(e) {
+  let title = document.getElementById('title').value.toLowerCase() ;
+  const word = { title };
 
-    let title = document.getElementById("title").value
-    //let description= document.getElementById("description").value
+  if (localStorage.getItem('words') === null) {
+    let words = [];
+    words.push(word);
+    localStorage.setItem('words', JSON.stringify(words));
+  } else {
+    let words = JSON.parse(localStorage.getItem('words'));
+    words.push(word);
+    localStorage.setItem('words', JSON.stringify(words));
+  }
 
-    //let lettersArr = title.split("")
-
-    const word = { title }
-
-
-    if (localStorage.getItem("words") === null) {
-        let words = []
-        words.push(word)
-        localStorage.setItem("words", JSON.stringify(words))
-    } else {
-        let words = JSON.parse(localStorage.getItem("words"))
-        words.push(word)
-        localStorage.setItem("words", JSON.stringify(words))
-    }
-
-    getTask()
-    document.getElementById("formWords").request()
-    e.preventDefault()
+  getTask();
+  document.getElementById('formWords').request();
+  e.preventDefault();
 }
 
 function getTask() {
+  let word = JSON.parse(localStorage.getItem('words'));
+  let tasksView = document.getElementById('tasks');
+  
+  let mapLettersAndRegex = new Map([
+    ['a', 'a@4'],
+    ['b', 'b38'],
+    ['c', 'co0'],
+    ['d', 'dbp'],
+    ['e', 'e3'],
+    ['f', 'f7'],
+    ['g', 'gp'],
+    ['h', 'h'],
+    ['i', 'i1l!¡'],
+    ['j', 'j7i'],
+    ['k', 'k<q'],
+    ['l', 'li¡!'],
+    ['m', 'mnw'],
+    ['n', 'nm'],
+    ['ñ', 'ñnm'],
+    ['o', 'o0'],
+    ['p', 'pbdq'],
+    ['q', 'qp'],
+    ['r', 'r7'],
+    ['s', 's$5'],
+    ['t', 't7'],
+    ['u', 'uv'],
+    ['v', 'vu'],
+    ['w', 'wv'],
+    ['x', 'xk'],
+    ['y', 'yv'],
+    ['z', 'z2'],
+    ['ç', 'ç'],
+    [' ', '.'],
+    ['á', 'á@4'],
+    ['é', 'é3'],
+    ['í', 'í1l!¡'],
+    ['ó', 'ó0'],
+    ['ú', 'úv'],
+    ['à', 'à@4'],
+    ['è', 'è3'],
+    ['ì', 'ì1l!¡'],
+    ['ò', 'ò0'],
+    ['ù', 'ùv'],
+  ]);
+  tasksView.innerHTML = '';
 
-    let word = JSON.parse(localStorage.getItem("words"))
-    let tasksView = document.getElementById("tasks")
-    let mapLettersAndRegex = new Map([["a", "a@4"], ["b", "b38"], ["c", "co0"], ["d", "dbp"], ["e", "e3"],
-    ["f", "f7"], ["g", "gp"], ["h", "h"], ["i", "i1l!¡"], ["j", "j7i"], ["k", "k<q"], ["l", "li¡!"],
-     ["m", "mnw"], ["n", "nm"],["ñ", "ñnm"], ["o", "o0"], ["p", "pbdq"], ["q", "qp"], ["r", "r7"],
-     ["s", "s$5"], ["t", "t7"],["u", "uv"], ["v", "vu"], ["w", "wv"], ["x", "xk"], ["y", "yv"], ["z", "z2"],
-     ["ç", "ç"], [" ", "."]])
-    tasksView.innerHTML = ""
-
-    for (let i = 0; i < word.length; i++) {
-        let title = word[i].title
-        let lettersArr = title.split("")
-        let wordRegexArr = []
-        for (let i = 0; i < lettersArr.length; i++) {
-            let letter = lettersArr[i]
-            mapLettersAndRegex.get(letter)
-            wordRegexArr.push(mapLettersAndRegex.get(letter))    
-        }
-        let wordRegex = wordRegexArr.toString().replaceAll(",", "][")
-        wordRegex = "[" + wordRegex + "]"
-        tasksView.innerHTML += `<div class="card">
+  for (let i = 0; i < word.length; i++) {
+    let title = word[i].title;
+    let lettersArr = title.split('');
+    let wordRegexArr = [];
+    for (let i = 0; i < lettersArr.length; i++) {
+      let letter = lettersArr[i];
+      mapLettersAndRegex.get(letter);
+      wordRegexArr.push(mapLettersAndRegex.get(letter));
+    }
+    let wordRegex = wordRegexArr.toString().replaceAll(',', '][');
+    wordRegex = '[' + wordRegex + ']';
+    tasksView.innerHTML += `<div class="card">
             <div class="card-body">
              <p>${title}</p>
              <p>${wordRegex}</p>
@@ -55,23 +85,21 @@ function getTask() {
              Delete
              </a>
             </div>
-        </div>`
-
-
-    }
+        </div>`;
+  }
 }
 
 function deleteTask(title) {
-    console.log(title)
-    let words = JSON.parse(localStorage.getItem("words"))
-    console.log(words)
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].title == title) {
-            words.splice(i, 1)
-        }
+  console.log(title);
+  let words = JSON.parse(localStorage.getItem('words'));
+  console.log(words);
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].title == title) {
+      words.splice(i, 1);
     }
-    localStorage.setItem("words", JSON.stringify(words))
-    getTask()
+  }
+  localStorage.setItem('words', JSON.stringify(words));
+  getTask();
 }
 
-getTask()
+getTask();
